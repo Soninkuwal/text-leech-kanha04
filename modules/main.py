89fc1ff6758071b3a3a0ed38fd58d5a676424bb4
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-04-07 20:33:48 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -166,7 +166,7 @@ async def account_login(bot: Client, m: Message):
                 ytf = f"b[height<={raw_text2}]/bv[height<={raw_text2}]+ba/b/bv+ba"
 
             if "jw-prod" in url:
-                cmd = f'yt-dlp -o "@Chatkanhabot {name}.mp4" "{url}"'
+                cmd = f'yt-dlp -o "{name}.mp4" "{url}"'
             else:
                 cmd = f'yt-dlp -f "{ytf}" "{url}" -o "@Chatkanhabot {name}.mp4"'
 
