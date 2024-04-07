c004db91e503ae768c9f59eeefa03fd951d9371d
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-04-07 20:44:34 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -168,7 +168,7 @@ async def account_login(bot: Client, m: Message):
             if "jw-prod" in url:
                 cmd = f'yt-dlp -o "{name}.mp4" "{url}"'
             else:
-                cmd = f'yt-dlp -f "{ytf}" "{url}" -o "@Chatkanhabot {name}.mp4"'
+                cmd = f'yt-dlp -f "{ytf}" "{url}" -o "{name}.mp4"'
 
             try:  
                 
@@ -198,6 +198,20 @@ async def account_login(bot: Client, m: Message):
                         await m.reply_text(str(e))
                         time.sleep(e.x)
                         continue
+
+                elif ".mp4" in url:
+                    try:
+                        cmd = f'yt-dlp -o "@Chatkanhabot {name}.mp4" "{url}"'
+                        download_cmd = f"{cmd} -R 25 --fragment-retries 25"
+                        os.system(download_cmd)
+                        copy = await bot.send_document(chat_id=m.chat.id, document=f'@Chatkanhabot {name}.mp4', caption=cc)
+                        count += 1
+                        os.remove(f'@Chatkanhabot {name}.mp4')
+                    except FloodWait as e:
+                        await m.reply_text(str(e))
+                        time.sleep(e.x)
+                        continue                
+                
                 else:
                     Show = f"❊⟱𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝𝐢𝐧𝐠 ⟱❊ »\n\n📝 𝐍𝐚𝐦𝐞 » `{name}\n\n⌨ 𝐐𝐮𝐥𝐢𝐭𝐲 » {raw_text2}`\n\n**🔗 𝐔𝐑𝐋 »** `😉`\n\n**𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃𝐄𝐃 𝐁𝐘 ➤ ▂▃▅▇█▓▒░❤️ 𝐈𝐏𝐒 𝐋𝐎𝐕𝐄𝐑'𝐒🌈™ ❤️░▒▓█▇▅▃▂ @Chatkanhabot **"
                     prog = await m.reply_text(Show)
