0b1cc25712683361f0d2721b07df99461ff11cc7
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-04-07 21:09:10 +0530

    Update core.py

--- modules/core.py
+++ modules/core.py
@@ -171,9 +171,9 @@ async def download_video(url,cmd, name):
         if os.path.isfile(f"{name}.mkv"):
             return f"{name}.mkv"
         elif os.path.isfile(f"{name}.mp4"):
-            return f"@Chatkanhabot {name}.mp4"
+            return f"{name}.mp4"
         elif os.path.isfile(f"{name}.mp4.webm"):
-            return f"@Chatkanhabot {name}.mp4.webm"
+            return f"{name}.mp4.webm"
 
         return name
     except FileNotFoundError as exc:
